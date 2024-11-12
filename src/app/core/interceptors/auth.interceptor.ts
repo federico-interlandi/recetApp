95534137b8d52697d2272fd76c9e21eafb2d332a
Interlandi <federico.interlandi@accenture.com>
2024-11-12 13:42:43 -0300

    /auth redirect when 400 error ocurres fixed

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,23 +1,26 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable()
 export class AuthInterceptorService implements HttpInterceptor {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private cookieService: CookieService, private ngZone: NgZone) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
         if (error.status >= 400 && error.status < 500) {
-          document.cookie = 'session=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
 
+          this.cookieService.delete('token')
           localStorage.clear();
+          // this.router.navigate(['/auth']);
 
-          this.router.navigate(['/auth']);
+          window.location.href = '/auth';
+
         }
         return throwError(error);
       })
